d3de17a8716859507e25fe3ec86199c26a535192
Pavel Parkhomey <pavelpark@aol.com>
2017-05-08 10:08:29 -0700

    Initial Commit

---
+++ ToDo-List/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDo-List
+//
+//  Created by Pavel Parkhomey on 5/8/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
