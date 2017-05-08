bf50c6c9715a4350979809dbc142aa31f743f448
Pavel Parkhomey <pavelpark@aol.com>
2017-05-08 11:22:34 -0700

    Added textfields to the login view controller.

---
+++ ToDo-List/LogInViewController.m
@@ -0,0 +1,27 @@
+//
+//  LogInViewController.m
+//  ToDo-List
+//
+//  Created by Pavel Parkhomey on 5/8/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import "LogInViewController.h"
+
+@interface LogInViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *emailTextFeild;
+
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextFeild;
+
+@end
+
+@implementation LogInViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+@end
