c64e2e22492ba58a56b3c3bb6ae9ee2cc2200061
Pavel Parkhomey <pavelpark@aol.com>
2017-05-10 10:23:05 -0700

    Added Credentials file

---
+++ TV-todo-list/Credentials.h
@@ -0,0 +1,14 @@
+//
+//  Credentials.h
+//  ToDo-List
+//
+//  Created by Pavel Parkhomey on 5/10/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#ifndef Credentials_h
+#define Credentials_h
+
+#define APP_KEY @"EPpGuaIC2v2HzX8jhgupnTt9ESh8ATWOxFcE5gWk"
+
+#endif /* Credentials_h */
